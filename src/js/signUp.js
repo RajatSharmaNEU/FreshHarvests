f6a14a8d6cdcc57fe8689a0209e7277e36672123
Rajat Sharma <sharma.raja@northeastern.edu>
2023-02-16 17:31:26 -0500

    Add validation and alert control

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -3,4 +3,26 @@ import '../scss/styles.scss';
 
 import * as bootstrap from 'bootstrap';
 
-console.log('Add SignUp Code');
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+const signUpForm  = document.getElementById('signup-form');
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper);
+
+    setTimeout( wrapper.remove, 2000);
+}
+
+document.querySelector('button[type="submit"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    if(signUpForm.checkValidity()) {
+        alert('Thank you, you sign up request has been raised!', 'success');
+    }
+})
