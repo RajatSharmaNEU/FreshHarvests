fa9fa2c45a0eb399436e9218aac2cdaaee1ba880
Nupoor Korde <korde.n@northeastern.edu>
2023-04-01 17:48:13 -0400

    Create directory frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.scss';
 import App from './components/App';
-import { applyMiddleware, compose, createStore } from 'redux';
 
 ReactDOM.render(
   <React.StrictMode>
