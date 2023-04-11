8e07f69d8b2ec60bb40eab26bc70a542b18fdb72
Rajat Sharma <sharma.raja@northeastern.edu>
2023-04-11 13:05:01 -0400

    Add basic routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import Signup from "./Signup/Signup";
 
-const App = () =>  {
+function App() {
     return (
-        <div>Hello World</div>
-    )
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/login" element={<Login/>}/>
+                <Route exact path="/register" element={<Register/>}/>
+                <Route exact path="/signup" element={<Signup/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
