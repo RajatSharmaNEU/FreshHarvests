5853c77c5e331902493eb72c5104f908de4e8f4c
Yash Pawar <pawar.ya@northeastern.edu>
2023-04-12 16:35:02 -0400

    Add dummy data

---
+++ frontend/src/API/placeOrder.js
@@ -0,0 +1,8 @@
+const placeOrder = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve({ status: 'success', message: 'Order placed successfully!!!' }), Math.random() * 2000);
+    setTimeout(() => reject({ status: 'danger', message: 'Payment failed. Please retry!!!' }), Math.random() * 2000);
+  });
+};
+
+export default placeOrder;
