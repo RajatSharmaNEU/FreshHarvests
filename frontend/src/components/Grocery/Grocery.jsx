41a376216906997e123a74cd04dbcfc9e50a9e06
Chakradhar-Grandhi <grandhidurga.c@northeastern.edu>
2023-04-12 17:59:11 -0400

    Integrate loader & notify component

--- frontend/src/components/Grocery/Grocery.jsx
+++ frontend/src/components/Grocery/Grocery.jsx
@@ -5,6 +5,8 @@ import Items from './Items';
 import Cart from './Cart';
 import {Container} from "react-bootstrap";
 import Navigation from "./Navigation";
+import Loader from "./Loader";
+import Notify from "./Notify";
 
 const Grocery = () => {
     const [cart, setCart] = useState(() => {
@@ -12,10 +14,14 @@ const Grocery = () => {
         return localCart ? JSON.parse(localCart) : [];
     });
     const [items, setItems] = useState([]);
+    const [loader, setLoader] = useState(false);
+    const [notify, setNotify] = useState(null);
 
     useEffect(() => {
+        setLoader(true);
         fetchMarketItems().then((result) => {
             setItems(result);
+            setLoader(false);
         });
     }, []);
 
@@ -46,12 +52,14 @@ const Grocery = () => {
     return (
         <Container fluid className="p-0">
             <Navigation cart={cart}/>
+            {loader && <Loader/>}
+            {notify && <Notify variant={notify.variant} message={notify.message}/>}
             <Routes>
                 <Route exact path="items"
                        element={<Items items={items} addItem={addItem} removeItem={removeItem}/>}
                 />
                 <Route exact path="cart"
-                       element={<Cart cart={cart} setCart={setCart}/>}/>
+                       element={<Cart cart={cart} setCart={setCart} setLoader={setLoader} setNotify={setNotify}/>}/>
                 <Route path="*" element={<Navigate to="items"/>}/>
             </Routes>
         </Container>
