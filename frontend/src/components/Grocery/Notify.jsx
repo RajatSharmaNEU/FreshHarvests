0789d4ce030e3fe8aed8f8f62846e1680a8a9f0f
Chakradhar-Grandhi <grandhidurga.c@northeastern.edu>
2023-04-12 17:58:41 -0400

    Add notify component

---
+++ frontend/src/components/Grocery/Notify.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import { string } from 'prop-types';
+
+const Notify = (props) => {
+  const { variant, message } = props;
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(variant && message);
+  }, [variant, message]);
+
+  const onClose = () => {
+    setShow(false);
+  };
+
+  console.log('Notify Re-rendered');
+  return (
+    show ?
+      <Alert variant={variant} className="text-center" onClose={onClose} dismissible>
+        {message}
+      </Alert> : null
+  );
+};
+
+Notify.propTypes = {
+  variant: string.isRequired,
+  message: string.isRequired,
+};
+
+export default Notify;
