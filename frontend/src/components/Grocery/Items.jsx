ba35916da3c7d66ed4b7bbd0bf2ec059bf2485c5
Chakradhar-Grandhi <grandhidurga.c@northeastern.edu>
2023-04-20 14:53:51 -0400

    Diplay loader while list is empty

--- frontend/src/components/Grocery/Items.jsx
+++ frontend/src/components/Grocery/Items.jsx
@@ -3,27 +3,38 @@ import {Col, Row} from 'react-bootstrap';
 import Item from './Item';
 import {arrayOf, func} from 'prop-types';
 import itemShape from "../../proptypes/item";
+import Loader from "./Loader";
 
 const Items = (props) => {
     const {items, addItem, removeItem, cart} = props;
     console.log('Items Re-rendered');
     return (
-        items && items.length > 0 ?
-            <Row className="mx-0">
-                {
-                    items.map(item => {
-                            const itemCount = cart.filter(cartItem => cartItem.name === item.name)[0]?.quantity;
-                            return (
-                                <Col md={3} key={item.name}>
-                                    <Item itemCount={itemCount} item={item} addItem={addItem} removeItem={removeItem}/>
-                                </Col>
-                            )
+        <>
+            {
+                (items === undefined || items === null) ? <Loader/> :
+                    <>
+                        {
+                            items.length > 0 ?
+                                <Row className="mx-0">
+                                    {
+                                        items.map(item => {
+                                                const itemCount = cart.filter(cartItem => cartItem.name === item.name)[0]?.quantity;
+                                                return (
+                                                    <Col md={3} key={item.name}>
+                                                        <Item itemCount={itemCount} item={item} addItem={addItem}
+                                                              removeItem={removeItem}/>
+                                                    </Col>
+                                                )
+                                            }
+                                        )
+                                    }
+                                </Row>
+                                : <h3 className="text-center text-danger">No Grocery Items available</h3>
                         }
-                    )
-                }
-            </Row>
-            : <h3 className="text-center text-danger">No Grocery Items available</h3>
-    );
+                    </>
+            }
+        </>
+    )
 };
 
 
